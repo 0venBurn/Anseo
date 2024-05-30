719fdbff5dd89b01ef46cdda4047e0458bf1c1e9
Evan <evanbyrnecode@gmail.com>
2024-05-30 14:25:03 +0100

    Configured React-Frontend file directory - Added react typescript frontend with vite - Configured Linting & formatting. - Run npm run lint and npm run lint:fix and npm run format for scripts - Added tailwind css - Added material UI dependencies

---
+++ React-Frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose on all network interfaces
+    port: 3000       // Set port to 3000
+  }
+})
