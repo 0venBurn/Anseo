f3550241d4d529872fac020b4522d0520e0dd7f4
Evan <evanbyrnecode@gmail.com>
2024-05-31 16:49:30 +0100

    Finalised Docker Configurations and started .env management - Added docker-compose files - Added Docker files - Added nginx Configurations - Added python config script to check for .env

--- Python-Flask-API/app/__init__.py
+++ Python-Flask-API/app/__init__.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 
 def create_app():
     app = Flask(__name__)
-    
+
+    # Load the .env file
+    load_dotenv()
+
+    # Determine the environment
+    env = os.getenv("ENV", "local")
+
+    if env == "local":
+        load_dotenv(".env.local")
+    elif env == "dev":
+        load_dotenv(".env.dev")
+    elif env == "prod":
+        load_dotenv(".env.prod")
+
     with app.app_context():
         from .routes.hello import hello_blueprint
+
         app.register_blueprint(hello_blueprint)
 
     return app
