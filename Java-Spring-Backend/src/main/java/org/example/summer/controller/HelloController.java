9249b0a379eefe6c3add087231ecb1c1d61b763c
Evan <evanbyrnecode@gmail.com>
2024-05-31 23:04:04 +0100

    Add full configuration and settings for .env files - Each docker container and docker-compose runs off .env.dev files - Local development ie regular testing runs off of base .env file - Deployment runs off of .env.prod - Configured test endpoints to display test .env variables working   accross different environments

--- Java-Spring-Backend/src/main/java/org/example/summer/controller/HelloController.java
+++ Java-Spring-Backend/src/main/java/org/example/summer/controller/HelloController.java
@@ -1,14 +1,19 @@
 package org.example.summer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
     @GetMapping("/hello")
     public String sayHello(){
-        return "Hello from Spring Backend!";
+        return "Hello from Spring Backend! Here is a environment variable: " + dbUsername;
     }
 
 }
--- Python-Flask-API/app/routes/hello.py
+++ Python-Flask-API/app/routes/hello.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-
+import os 
 hello_blueprint = Blueprint("hello", __name__)
 
 @hello_blueprint.route("/hello", methods=["Get"])
 def hello():
-    return "Hello, from the API!"
+    db_host = os.getenv('DB_HOST')
+    return f"Hello, from the API!, Here is a {db_host}"
