a5d1d219688dc9439c7c3438fb7eee27c00e70cf
Tadgh Purcell <tadghp@gmail.com>
2024-06-10 15:47:19 +0100

    Add graphQL server to backend and update taxiZones to zipCodes

---
+++ Java-Spring-Backend/src/main/java/org/example/summer/controller/ZipCodeController.java
@@ -0,0 +1,26 @@
+package org.example.summer.controller;
+
+import org.example.summer.dao.ZipCodesRepository;
+import org.example.summer.entity.ZipCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/zipCodes")
+@CrossOrigin
+public class ZipCodesController {
+
+    @Autowired
+    private ZipCodesRepository zipCodesRepository;
+
+    @GetMapping("/all")
+    public List<ZipCode> getAllZipCodes() {
+        return zipCodesRepository.findAll(Sort.by(Sort.Direction.ASC, "zipCode"));
+    }
+}
