4996d3aecac216badc7a7d246c884091ff59bd99
0venburn <evanbyrnecode@gmail.com>
2024-06-12 10:31:42 +0100

    Updated Vite configuration for frontend

--- React-Frontend/vite.config.ts
+++ React-Frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose on all network interfaces
+    port: 3000       // Set port to 3000
+  }
 })
