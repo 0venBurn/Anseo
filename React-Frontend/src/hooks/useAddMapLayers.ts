984fb68a23baaaa43d8710cf7b18152ae8c7755b
Tadgh Purcell <tadghp@gmail.com>
2024-06-13 13:08:42 +0100

    Update map to use vectors instead of geojson and add filtering options. Also update the map from useRef to useState to fix loading issues.

--- React-Frontend/src/hooks/useAddMapLayers.ts
+++ React-Frontend/src/hooks/useAddMapLayers.ts
@@ -1,53 +1,40 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import mapboxgl from 'mapbox-gl';
-import { useGetLayers } from "./useGetLayers";
-import { GeoJson } from "../types/GeoJson";
 
 export const useAddMapLayers = (map: mapboxgl.Map | null) => {
-    const { layers } = useGetLayers();
-    const layersAddedRef = useRef(false);
 
     useEffect(() => {
-        if (map && layers && !layersAddedRef.current) {
-            const addLayers = () => {
-                if (layers.features.length > 0) {
-                    if (!map.getSource("Layers")) {
-                        map.addSource("Layers", {
-                            'type': 'geojson',
-                            'data': layers as any
-                        });
+        if (map) {
+                map.addSource("Layers", {
+                    type: 'vector',
+                    url: 'mapbox://tadghp.7vbqyjvf'
+            })
 
-                        map.addLayer({
-                            'id': "LayersFill",
-                            'type': 'fill',
-                            'source': "Layers",
-                            'layout': {},
-                            'paint': {
-                                'fill-color': '#0080ff',
-                                'fill-opacity': 0.5
-                            }
-                        });
+                map.addLayer({
+                    'id': "LayersFill",
+                    'type': 'fill',
+                    'source': "Layers",
+                    'source-layer': "NeighborhoodsBoundries",
+                    layout: {},
+                    'paint': {
+                                    'fill-color': '#0080ff',
+                                    'fill-opacity': 0.5
+                    },
+                    'filter': ['==', ['get', 'boroname'], 'Brooklyn']
+            });
 
-                        map.addLayer({
-                            'id': "LayersOutline",
-                            'type': 'line',
-                            'source': "Layers",
-                            'layout': {},
-                            'paint': {
-                                'line-color': '#000',
-                                'line-width': 0.5
-                            }
-                        });
-                    }
-                }
-                layersAddedRef.current = true;
-            };
-
-            if (map.isStyleLoaded()) {
-                addLayers();
-            } else {
-                map.on('load', addLayers);
-            }
+                map.addLayer({
+                    'id': "LayersOutline",
+                    'type': 'line',
+                    'source': "Layers",
+                    'source-layer': "NeighborhoodsBoundries",
+                    'layout': {},
+                    'paint': {
+                        'line-color': '#000',
+                        'line-width': 0.5
+                    },
+                    'filter': ['==', ['get', 'boroname'], 'Brooklyn']
+            });
         }
-    }, [map, layers]);
+    }, [map]);
 };
