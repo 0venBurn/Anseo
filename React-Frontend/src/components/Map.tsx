3a7ac52580cb18bdc06415c45640fded4b8dff50
Tadgh Purcell <tadghp@gmail.com>
2024-06-13 13:08:42 +0100

    Update map to use vectors instead of geojson and add filtering options. Also update the map from useRef to useState to fix loading issues.

--- React-Frontend/src/components/Map.tsx
+++ React-Frontend/src/components/Map.tsx
@@ -4,7 +4,7 @@ import "../App.css";
 
 export const Map = () => {
     const { mapRef, map } = useMapInit(40.7075272168033, -74.00663048205502, 10);
-    useAddMapLayers(map.current);
+    useAddMapLayers(map);
 
     return <div ref={mapRef} className="map" style={{ width: '100%', height: '100vh' }} />;
 };
