6f1d79af2b78deadac881428ec0094918b8d7d34
zkw-pra <157477084+zkw-pra@users.noreply.github.com>
2024-06-14 10:40:31 +0100

    divide polygons to 5 boroughs and display one specific borough

--- React-Frontend/src/components/Map.tsx
+++ React-Frontend/src/components/Map.tsx
@@ -1,12 +1,10 @@
 import { useMapInit } from "../hooks/useMapInit";
-import { useGetTaxiZones } from "../hooks/useGetTaxiZones";
 import { useAddMapLayers } from "../hooks/useAddMapLayers";
-import '../index.css';
+import "../index.css";
 
 export const Map = () => {
     const { mapRef, map } = useMapInit(40.7075272168033, -74.00663048205502, 10);
-    const { taxiZones } = useGetTaxiZones(map.current, mapRef.current);
-    useAddMapLayers(map.current, taxiZones, mapRef.current);
+    useAddMapLayers(map);
 
     return <div ref={mapRef} className="map" style={{ width: '100%', height: '100vh' }} />;
 };
--- React-Frontend/src/hooks/useAddMapLayers.ts
+++ React-Frontend/src/hooks/useAddMapLayers.ts
@@ -1,56 +1,40 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import mapboxgl from 'mapbox-gl';
-import { Zone } from "../types/GeoJson";
 
-export const useAddMapLayers = (map: mapboxgl.Map | null, taxiZones: Zone[], mapRef: HTMLDivElement | null) => {
-    const layersAddedRef = useRef(false)
+export const useAddMapLayers = (map: mapboxgl.Map | null) => {
 
     useEffect(() => {
-        if (map && !layersAddedRef.current) {
-            const addLayers = () => {
-                if (taxiZones.length > 0) {
-                    taxiZones.forEach(zone => {
-                        if (!map.getSource(`${zone.properties.zip_code}`)) {
-                            map.addSource(`${zone.properties.zip_code}`, {
-                                'type': 'geojson',
-                                'data': {
-                                    'type': 'Polygon',
-                                    'coordinates': zone.geometry.coordinates
-                                }
-                            });
-                            map.addLayer({
-                                'id': `${zone.properties.zip_code}`,
-                                'type': 'fill',
-                                'source': `${zone.properties.zip_code}`,
-                                'layout': {},
-                                'paint': {
+        if (map) {
+                map.addSource("Layers", {
+                    type: 'vector',
+                    url: 'mapbox://tadghp.7vbqyjvf'
+            })
+
+                map.addLayer({
+                    'id': "LayersFill",
+                    'type': 'fill',
+                    'source': "Layers",
+                    'source-layer': "NeighborhoodsBoundries",
+                    layout: {},
+                    'paint': {
                                     'fill-color': '#0080ff',
                                     'fill-opacity': 0.5
-                                }
-                            });
-                            map.addLayer({
-                                'id': `Outline: ${zone.properties.zip_code}`,
-                                'type': 'line',
-                                'source': `${zone.properties.zip_code}`,
-                                'layout': {},
-                                'paint': {
-                                    'line-color': '#000',
-                                    'line-width': 0.5
-                                }
-                            });
-                        }
-                    });
-                } 
-                layersAddedRef.current = true;
-            };
+                    },
+                    'filter': ['==', ['get', 'boroname'], 'Brooklyn']
+            });
 
-            addLayers()
-
-            // if (map.isStyleLoaded()) {
-            //     addLayers()
-            // } else {
-            //     map.on('load', addLayers)
-            // }
+                map.addLayer({
+                    'id': "LayersOutline",
+                    'type': 'line',
+                    'source': "Layers",
+                    'source-layer': "NeighborhoodsBoundries",
+                    'layout': {},
+                    'paint': {
+                        'line-color': '#000',
+                        'line-width': 0.5
+                    },
+                    'filter': ['==', ['get', 'boroname'], 'Brooklyn']
+            });
         }
-    }, [map, taxiZones, mapRef]);
+    }, [map]);
 };
