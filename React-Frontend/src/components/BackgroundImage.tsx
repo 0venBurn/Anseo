f162693452ef15ecb948dcb5283c0c40ac163168
0venburn <evanbyrnecode@gmail.com>
2024-06-15 14:07:41 +0100

    Added Background image component
    - Added Background image component from the landing page
    - Makes background image more reusable
    - Makes section regarding background component easier to find
    - Allows for testing of component

---
+++ React-Frontend/src/components/BackgroundImage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BackgroundImage: React.FC = () => (
+  <img
+    src="..public/img/landing.png"
+    alt="Background"
+    className="absolute inset-0 w-full h-full object-cover opacity-80"
+  />
+);
+
+export default BackgroundImage;
