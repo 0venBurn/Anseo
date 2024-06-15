c9a58892bc9102e4a9f62e7b0d1cfbf055956bb2
0venburn <evanbyrnecode@gmail.com>
2024-06-15 17:05:40 +0100

    Corrected import paths and image references

--- React-Frontend/src/components/BackgroundImage.tsx
+++ React-Frontend/src/components/BackgroundImage.tsx
@@ -12,7 +12,7 @@ import React from "react";
 
 const BackgroundImage: React.FC = () => (
   <img
-    src="..public/img/landing.png"
+    src="/img/landing.png"
     alt="Background"
     className="absolute inset-0 w-full h-full object-cover opacity-80"
   />
