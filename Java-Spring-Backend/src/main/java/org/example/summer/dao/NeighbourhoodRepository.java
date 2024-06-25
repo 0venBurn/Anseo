1ad245457ad287a321ebcd62f5e456d44c4566be
Tadgh Purcell <tadghp@gmail.com>
2024-06-25 15:29:01 +0100

    Add CORS for neighbourhoods

--- Java-Spring-Backend/src/main/java/org/example/summer/dao/NeighbourhoodRepository.java
+++ Java-Spring-Backend/src/main/java/org/example/summer/dao/NeighbourhoodRepository.java
@@ -2,6 +2,10 @@ package org.example.summer.dao;
 
 import org.example.summer.entity.Neighbourhood;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@Repository
+@CrossOrigin
 public interface NeighbourhoodRepository extends JpaRepository<Neighbourhood, Integer> {
 }
