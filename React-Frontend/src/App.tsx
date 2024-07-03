1663969fd9b66f0968faca7b9f01389c73f34679
Tadgh Purcell <tadghp@gmail.com>
2024-07-03 17:58:07 +0100

    Update Routing

--- React-Frontend/src/App.tsx
+++ React-Frontend/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import "./App.css";
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
 import ErrorPage from "./pages/ErrorPage";
-import SignedIn from "./pages/SignedIn";
+import LandingPage from './LandingPage';
+import QuestionPage from './QuestionPage';
+import BoroughPage from './BoroughPage';
+import WelcomePage from './WelcomePage';
+import SubmitPage from './SubmitPage';
+import LoginPage from './LoginPage';
+import SignInPage from './SignInPage';
+import AboutPage from './AboutPage';
+import MapPage from './MapPage';
+import TargetPage from './TargetPage';
+import AreaPage from './AreaPage';
+import ExtraPage from './ExtraPage';
+import PasswordPage from './PasswordPage';
+import TestPage from './SubmitPageTest';
+
 import AuthenticationProvider from "./components/AuthenticationProvider";
 
 const AuthLayout = () => (
@@ -20,12 +32,72 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <HomePage />,
+        element: <LandingPage />,
         errorElement: <ErrorPage />,
       },
       {
-        path: "/signedin",
-        element: <SignedIn />,
+        path: "/questions",
+        element: <QuestionPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/borough",
+        element: <BoroughPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/welcome",
+        element: <WelcomePage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/submit",
+        element: <SubmitPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/signin",
+        element: <SignInPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/map",
+        element: <MapPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/target",
+        element: <TargetPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/area",
+        element: <AreaPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/extra",
+        element: <ExtraPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/password",
+        element: <PasswordPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/test",
+        element: <TestPage />,
         errorElement: <ErrorPage />,
       }
     ]
@@ -42,10 +114,4 @@ const App: React.FC = () => {
   );
 };
 
-const AppWrapper: React.FC = () => (
-  <Router>
-    <App />
-  </Router>
-);
-
-export default AppWrapper;
+export default App;
--- React-Frontend/src/hooks/useMapInit.ts
+++ React-Frontend/src/hooks/useMapInit.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { useEffect, useRef, useState } from "react";
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../mapbox.config';
 
@@ -8,13 +7,8 @@ mapboxgl.accessToken = environment.mapbox.accessToken
 export const useMapInit = (lat: number, lng: number, zoom: number) => {
     const mapRef = useRef<HTMLDivElement | null>(null);
     const [map, setMap] = useState<mapboxgl.Map | null>(null);
-    const [map, setMap] = useState<mapboxgl.Map | null>(null);
     
     useEffect(() => {
-        if (mapRef.current && !map) {
-            let mapInstance: mapboxgl.Map | null = null;
-
-            mapInstance = new mapboxgl.Map({
         if (mapRef.current && !map) {
             let mapInstance: mapboxgl.Map | null = null;
 
@@ -22,27 +16,10 @@ export const useMapInit = (lat: number, lng: number, zoom: number) => {
                 container: mapRef.current,
                 center: [lng, lat],
                 zoom,
-                pitchWithRotate: true,
-                dragRotate: true,
-                style: 'mapbox://styles/zikangwang/clxou0aln00m201qw6txr0ydv',
+                pitchWithRotate: false,
+                style: 'mapbox://styles/tadghp/clxbpfz0f025901pce9n78onm/draft',
             }); 
 
-            mapInstance.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-
-            mapInstance.on('load', () => {
-                setMap(mapInstance);
-            })
-            
-            return () => {
-                if (mapInstance) {
-                    mapInstance.remove();
-                }
-                if (map) {
-                    (map as mapboxgl.Map).remove();
-                }
-            };
-            }  
-    }, [lat, lng, zoom]);
             mapInstance.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
             mapInstance.on('load', () => {
