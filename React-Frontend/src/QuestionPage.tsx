fd207a12a9f84c7238b6cc9f3ea34fa68ecbd76a
zkw-pra <157477084+zkw-pra@users.noreply.github.com>
2024-07-03 13:43:41 +0100

    input to model now working, and updated webpage based off figma

--- React-Frontend/src/QuestionPage.tsx
+++ React-Frontend/src/QuestionPage.tsx
@@ -107,7 +107,7 @@ const QuestionPage: React.FC = () => {
                   onChange={(e) => setCloseHour(parseInt(String(e.target.value)))}
                   className="w-full rounded-lg py-2 px-4 text-xl font-bold border-2 bg-white"
                 >
-                  {[...Array(24).keys()].filter(hour => hour >= openHour).map(hour => (
+                  {[...Array(24).keys()].map(hour => (
                     <MenuItem key={hour} value={hour}>{hour}:00</MenuItem>
                   ))}
                 </Select>
