c14878cc4f9b3d2a10a004698c9c0ab6a5336d14
0venburn <evanbyrnecode@gmail.com>
2024-07-09 18:39:22 +0100

    updated docker

--- Python-Flask-API/app/routes/hello.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-import os 
-hello_blueprint = Blueprint("hello", __name__)
-
-@hello_blueprint.route("/hello", methods=["Get"])
-def hello():
-    db_host = os.getenv('DB_HOST')
-    return f"Hello, from the API!, Here is a {db_host}"
