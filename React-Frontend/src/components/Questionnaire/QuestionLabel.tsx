2e07e2dcd7171f11fe02cc45599188ceb376a5ea
0venburn <evanbyrnecode@gmail.com>
2024-07-09 18:42:55 +0100

    fixed merge issue

--- React-Frontend/src/components/Questionnaire/QuestionLabel.tsx
+++ React-Frontend/src/components/Questionnaire/QuestionLabel.tsx
@@ -7,11 +7,5 @@ interface QuestionLabelProps {
 const QuestionLabel: React.FC<QuestionLabelProps> = ({ label }) => {
   return <h1 className="text-3xl mb-6 font-alegreya">{label}</h1>;
 };
-    return (
-    <h1 className="text-3xl mb-6 font-alegreya">
-        {label}
-      </h1>)
-}
 
 export default QuestionLabel;
-
