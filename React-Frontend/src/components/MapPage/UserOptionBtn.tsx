7f119e284ca683ae246b74db9771ae67de3503c7
Tadgh Purcell <tadghp@gmail.com>
2024-07-21 08:31:04 +0100

    Update user options header

---
+++ React-Frontend/src/components/MapPage/UserOptionBtn.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@mui/material';
+import { motion } from 'framer-motion';
+
+interface UserOptionBtnProps {
+    title: string;
+    isActive?: boolean;
+    handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const UserOptionBtn: React.FC<UserOptionBtnProps> = ({ title, isActive, handleClick }) => {
+    return (
+        <>
+        <Button 
+        component={motion.button}
+        whileHover={{ 
+            scale: 1.05 ,
+        }}
+        animate={{
+            backgroundColor: isActive ? '#D1D6F5' : 'inherit',
+            color: isActive ? '#3B447A' : '#ABB0B4',
+            transition: { duration: 0.1, ease: "easeInOut" },
+        }}
+        sx={{
+            fontFamily: 'Commissioner',
+            borderRadius: 4,
+            fontSize: '1rem',
+            textTransform: 'none',
+            fontWeight: isActive ? 600 : 500,
+        }}
+        className={isActive ? 'active' : ''}
+        onClick={handleClick}>
+            {title}
+        </Button>
+        </>
+    )
+}
+export default UserOptionBtn;
--- React-Frontend/src/components/MapPage/UserOptionsHeader.tsx
+++ React-Frontend/src/components/MapPage/UserOptionsHeader.tsx
@@ -1,14 +1,34 @@
-import { Button } from "@mui/material";
+import { useState } from "react";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import { useUser } from '@clerk/clerk-react';
+import UserOptionBtn from "./UserOptionBtn";
 
 const UserOptionsHeader: React.FC = () => {
+  const { user } = useUser();
+  const [activeBtn, setActiveBtn] = useState<string | null>('Favourites');
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setActiveBtn(e?.currentTarget.textContent);
+  }
+
     return (
         <div
-          className="w-full flex bg-user-sidebar-purple-dark justify-between items-center 
-          text-2xl py-2 px-4">
-          <span className="font-alegreya text-primary-text-dark font-bold">
-            Your Results
-          </span>
-          <Button variant="outlined">Filters</Button>
+          className="w-full flex bg-white items-center text-2xl py-2 px-4">
+            <SignedIn>
+              <div className="font-commissioner text-primary-text-dark px-2
+              after:content-['|'] after:text-shaded-grey after:w-[1px] after:h-full after:ml-2">{user?.username}</div>
+              <div className="flex items-center justify-end w-full h-full gap-4">
+                <UserOptionBtn 
+                  title="Favourites" 
+                  isActive={activeBtn === 'Favourites'}
+                  handleClick={handleClick}/>
+                <UserOptionBtn 
+                  title="Previous Searches" 
+                  isActive={activeBtn === 'Previous Searches'}
+                  handleClick={handleClick}/>
+              </div>
+            </SignedIn>
+            <SignedOut></SignedOut>
       </div>
     )
 }
