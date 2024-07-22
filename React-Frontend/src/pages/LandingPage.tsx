5b5e5ebadf93fdf3cd2775aba2b25779634ca246
sadhbh23 <127418826+sadhbh23@users.noreply.github.com>
2024-07-22 16:53:08 +0100

    Implemented react-toastify notifications to enhance UI

--- React-Frontend/src/pages/LandingPage.tsx
+++ React-Frontend/src/pages/LandingPage.tsx
@@ -1,14 +1,37 @@
-import Chat from "../components/Chatbox/Chat";
-
 // import necessary components
-import LandingPageHeroSection from "../components/LandingPage/LandingPageHeroSection";
-import "../index.css";
+import React, { useEffect } from "react";
 
 import LandingPageActionButtons from "../components/LandingPage/LandingPageActionButtons";
 import LandingPageHeader from "../components/LandingPage/LandingPageHeader";
+import LandingPageHeroSection from "../components/LandingPage/LandingPageHeroSection";
+import Chat from "../components/Chatbox/Chat";
+import "../index.css";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 //Main component for the landing page
 const LandingPage: React.FC = () => {
+  useEffect(() => {
+    toast("Welcome to Anseo! We're glad to have you here.", {
+      toastId: "welcome-toast",
+    });
+
+    setTimeout(() => {
+      toast("Get started by clicking the 'Start Now' button!", {
+        toastId: "start-now-toast",
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      toast(
+        "To check out our AI chat assistant click the expandable widget on the bottom right of the screen.",
+        {
+          toastId: "chat-assistant-toast",
+        }
+      );
+    }, 4000);
+  }, []);
+
   return (
     <>
       <div className="min-h-full flex flex-col bg-landingPageBackground object-contain img-fit ">
@@ -23,6 +46,18 @@ const LandingPage: React.FC = () => {
         </div>
       </div>
       <Chat />
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+      />
     </>
   );
 };
